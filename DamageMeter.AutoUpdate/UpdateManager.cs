5eda7563f0ff8abba42a18b903f699a224726a1d
Dzon <jihonguser@gmail.com>
2021-09-29 17:28:01 +0900

    Version: 210929

--- DamageMeter.AutoUpdate/UpdateManager.cs
+++ DamageMeter.AutoUpdate/UpdateManager.cs
@@ -17,7 +17,7 @@ namespace DamageMeter.AutoUpdate
     {
         private static Dictionary<string, string> _hashes;
         private static Dictionary<string, string> _latest;
-        public static string Version = "3.00b_210827";
+        public static string Version = "3.00b_210929";
 
         public static string ExecutableDirectory => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
